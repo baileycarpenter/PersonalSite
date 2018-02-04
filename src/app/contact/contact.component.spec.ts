593de9fefa493603bcc278475ab575956d6fb0eb
Keil Carpenter <keilcarpenter@protonmail.com>
2018-02-05 10:16:01 +1300

    configure contact.component.spec.ts testing module and import HttpTestingController and store in variable

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -1,16 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, inject, TestBed} from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ContactComponent } from './contact.component';
 import {FormBuilder, FormControl} from '@angular/forms';
 import {ValidationService} from './validation.service';
 import {ContactService} from './contact.service';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {HttpResponse} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {HttpResponse, HttpClient, HttpHeaders} from '@angular/common/http';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async(() => {
     // service stubs for testing
@@ -20,7 +21,7 @@ describe('ContactComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ ContactComponent ],
       schemas: [ NO_ERRORS_SCHEMA ],
-      imports: [ RouterTestingModule, HttpClientTestingModule],
+      imports: [ RouterTestingModule, HttpClientTestingModule ],
       providers: [
         FormBuilder,
         { provide: ValidationService, useValue: validationServiceStub },
@@ -30,12 +31,23 @@ describe('ContactComponent', () => {
     .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(
+    async(
+      inject([HttpTestingController], _httpMock => {
+        fixture = TestBed.createComponent(ContactComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        httpMock = _httpMock;
+      })
+    )
+  );
 
-    fixture = TestBed.createComponent(ContactComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  afterEach(
+    inject([HttpTestingController], (httpMock: HttpTestingController) => {
+      httpMock.verify();
+    })
+  );
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
---
+++ src/app/contact/contact.service.spec.ts
@@ -0,0 +1,20 @@
+/*
+import { TestBed, inject } from '@angular/core/testing';
+
+import { ContactService } from './contact.service';
+import {MockBackend} from '@angular/common/http';
+
+describe('ContactService', () => {
+  let service: ContactService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ContactService, MockBackend]
+    });
+  });
+
+  it('should be created', inject([ContactService], (service: ContactService) => {
+    expect(service).toBeTruthy();
+  }));
+});
+*/
