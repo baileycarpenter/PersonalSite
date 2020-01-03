55f8b564ba383d68a3fd848c23cb053268bb6210
Blasphemien <blasphemien@protonmail.com>
2020-01-03 17:16:24 +1300

    Fix breaking updates from Angular 5.2.1 => 8.2.14

--- src/app/contact/contact.service.ts
+++ src/app/contact/contact.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
 import {HttpHeaders, HttpClient} from '@angular/common/http';
 
 @Injectable()
